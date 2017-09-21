ca94c7d398475129680830b91f1dd2affb0fe5a4
jbadan <jenna.badanowski@gmail.com>
2017-09-21 09:31:59 -0700

    added ejs page

--- index.js
+++ index.js
@@ -5,26 +5,34 @@ var request = require('request');
 var app = express();
 
 app.use(express.static(__dirname + '/static'));
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 app.set('view engine','ejs');
-
-
 
 app.get('/', function(req, res){
   res.sendFile('./index.html');
 })
 
+// app.get('/jenna', function(req, res){
+//   res.render('./index.ejs');
+// })
+
 app.get('/search/:foo', function(req, res) {
   var url = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&';
-  var q = req.params.foo;
-  var fullUrl = url + 'q=' + q;
+  // var q = req.params.foo;
+  // var fullUrl = url + 'q=' + q;
   request({
-    url: fullUrl
+    url: url,
+    qs: {
+      limit: 20,
+      api_key: 'dc6zaTOxFJmzC',
+      q: req.params.foo
+    },
+    json: true
   }, function(error, response, body) {
-    var dataObj = JSON.parse(body);
-    // res.render('index', {data: dataObj});
-    res.send(dataObj);
+    // var dataObj = JSON.parse(body);
+    // res.send(dataObj);
+    // res.send(body.data);
+    res.render('index', {data: body.data});
   });
 });
 
